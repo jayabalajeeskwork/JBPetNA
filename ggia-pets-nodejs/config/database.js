4a36bfb183f84bee33a75db230c606c6bc9337d6
SanthakumarRavi <santhakumar122ravi@gmail.com>
2026-05-30 11:53:43 +0530

    done by santha

--- ggia-pets-nodejs/config/database.js
+++ ggia-pets-nodejs/config/database.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const mongoose = require("mongoose");
 const env = process.env;
 let connection = `${env.DB_CONNECTION}://${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`;
@@ -6,22 +6,24 @@ let connection = `${env.DB_CONNECTION}://${env.DB_HOST}:${env.DB_PORT}/${env.DB_
 //     connection = env.DB_SRV
 // }
 
-console.log(connection)
-mongoose.connect(connection, {
+console.log(connection);
+mongoose
+  .connect(connection, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
-    useUnifiedTopology: true
-}).catch(err => {
+    useUnifiedTopology: true,
+  })
+  .catch((err) => {
     console.error("Mongoose connection error:", err);
-});
+  });
 
-mongoose.connection.on('error', () => console.log);
-mongoose.connection.on('open', () => console.log("Connected to mongo server."));
+mongoose.connection.on("error", () => console.log);
+mongoose.connection.on("open", () => console.log("Connected to mongo server."));
 
 mongoose.Promise = Promise;
 
 module.exports = {
-    mongoose,
-    connection
+  mongoose,
+  connection,
 };
